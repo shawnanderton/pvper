1de6771ecd761d9a705c725a4022e7850a6b8fd9
Shawn Anderton <shawnanderton1@gmail.com>
2020-06-08 13:30:05 -0600

    fixed cache

--- api/shared/cache.js
+++ api/shared/cache.js
@@ -1,7 +1,14 @@
 const redis = require("redis");
 const { promisify } = require("util");
-
-const client = redis.createClient();
+let client = null;
+if (process.env.DEV) {
+  client = redis.createClient();
+} else {
+  client = redis.createClient(6379, process.env.REDISCACHEHOSTNAME, {
+    auth_pass: process.env.REDISCACHEKEY,
+    tls: { servername: process.env.REDISCACHEHOSTNAME },
+  });
+}
 const getAsync = promisify(client.get).bind(client);
 let expireTime = 600;
 

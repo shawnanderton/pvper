073bb2fd9b0a79c2537b7e3bed56c469aeadfe2a
Shawn Anderton <shawnanderton1@gmail.com>
2020-06-09 17:21:24 -0600

    another test

--- api/shared/cache.js
+++ api/shared/cache.js
@@ -1,25 +1,25 @@
-const redis = require("redis");
-const { promisify } = require("util");
-let client = null;
-if (process.env.DEV) {
-  client = redis.createClient();
-} else {
-  client = redis.createClient(6379, process.env.REDISCACHEHOSTNAME, {
-    auth_pass: process.env.REDISCACHEKEY,
-    tls: { servername: process.env.REDISCACHEHOSTNAME },
-  });
-}
-const getAsync = promisify(client.get).bind(client);
-let expireTime = 600;
+// const redis = require("redis");
+// const { promisify } = require("util");
+// let client = null;
+// if (process.env.DEV) {
+//   client = redis.createClient();
+// } else {
+//   client = redis.createClient(6379, process.env.REDISCACHEHOSTNAME, {
+//     auth_pass: process.env.REDISCACHEKEY,
+//     tls: { servername: process.env.REDISCACHEHOSTNAME },
+//   });
+// }
+// const getAsync = promisify(client.get).bind(client);
+// let expireTime = 600;
 
-const getJson = async (key) => {
-  const data = await getAsync(key);
-  return JSON.parse(data);
-};
+// const getJson = async (key) => {
+//   const data = await getAsync(key);
+//   return JSON.parse(data);
+// };
 
-const setJson = (key, value) => {
-  client.set(key, JSON.stringify(value), () => {});
-  client.expire(key, expireTime);
-};
+// const setJson = (key, value) => {
+//   client.set(key, JSON.stringify(value), () => {});
+//   client.expire(key, expireTime);
+// };
 
-module.exports = { getJson, setJson };
+// module.exports = { getJson, setJson };
